9fb136338d0fba2b2ddb412bb22a842ecc5ecf80
오준후 <ojh789ojh@naver.com>
2022-10-07 16:48:05 +0900

    충돌커밋

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,7 @@ public class Calculator{
     public static void main(String[] args) {
         int num1, num2, result=0;
         char operator;
-
+        //나는 충돌을 사랑하는 얼티메이트 디럭스 리뉴얼 하이퍼 범퍼카mk2 오메가!!!
         System.out.print("enter expression(1 + 2) : ");
         try (Scanner scanner = new Scanner(System.in)) {
             num1 = scanner.nextInt();
