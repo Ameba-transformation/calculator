9eef0b73984ef1671b0fed71609e303a10686ccd
장준영 <jjy0372@gmail.com>
2022-10-07 14:47:16 +0900

    #2022-git-gwangju10/1 스켈레톤

---
+++ Calculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args) {
+        int a, b, c=0;
+        char operator;
+
+        System.out.println("enter expression");
+        Scanner scanner = new Scanner(System.in);
+
+        a = scanner.nextInt();
+        operator = scanner.next().toCharArray()[0];
+        b = scanner.nextInt();
+
+        switch(operator){
+            case '+':c = add(a,b);
+                break;
+            case '-':c = sub(a,b);
+            break;
+            case '/':c = div(a,b);
+            break;
+            case '*':c = mul(a,b);
+            break;
+        }
+
+        System.out.println(c);
+
+    }
+
+    private static int sub(int a, int b) {
+        return 0;
+    }
+
+    private static int div(int a, int b) {
+        return 0;
+    }
+
+    private static int mul(int a, int b) {
+        return 0;
+    }
+
+    private static int add(int a, int b) {
+        return 0;
+    }
+}
